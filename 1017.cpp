a509b383ced971d493bf499fc69a10d1e01a533c
ghostlo <710340998@qq.com>
2018-03-09 14:24:21 +0800

    Create 1017.cpp

---
+++ 1017.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+char a[1001];
+char q[1001];
+int b;
+int r;
+int main()
+{
+    scanf("%s %d", a, &b);
+    int i,j;
+    for(i = 0, j = 0; a[i]; ++i) {
+        r = r * 10;
+        r += a[i] - '0';
+        q[j++] = (r / b) + '0';
+        r = r % b;
+    }
+    q[j] = '\0';
+    i = 0;
+    if(j != 1)
+        for(i = 0; q[i]=='0'; ++i);
+    cout<<q+i<<" "<<r<<endl;
+    return 0;
+}
