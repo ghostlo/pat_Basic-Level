c2a0ca96384aeaf06d0ff78792f3b9f47c6386f7
ghostlo <710340998@qq.com>
2018-03-10 18:22:24 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,12 +3,11 @@ from lxml import etree
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36',
 	'Content-Type': 'application/x-www-form-urlencoded',
-	'Referer': 'http://10.0.0.55:803/srun_portal_pc.php?ac_id=1&',
 	'Accept-Encoding': 'gzip, deflate',
 	'Accept-Language': 'zh-CN,zh;q=0.8'
 }
-q=raw_input("please input your user name")
-point=raw_input("please input your point(roughly)")
+q=raw_input("Please input your user name:")
+z=raw_input("Please input your correct test number(roughly):")
 for k in range(1,702,1):
 	em = [' ' for x in range(50)]
 	em1 = [' ' for x in range(50)]
@@ -21,15 +20,15 @@ for k in range(1,702,1):
 	resp.encoding = resp.apparent_encoding
 	r = resp.text
 	contentTree = etree.HTML(r, parser=etree.HTMLParser(encoding='utf-8'))
-	em4 = contentTree.xpath('//*[@id="ranklist"]/tbody/tr[1]/td[5]/text()')
-	print em4[0]+'the page is'+str(k)
-	m=float(em4[0])
-	n=int(point)
-	if(m<n+100):
+	em4 = contentTree.xpath('//*[@id="ranklist"]/tbody/tr[1]/td[4]/text()')
+	print em4[0]+' The page is : '+str(k)
+	m=int(em4[0])
+	n=int(z)
+	if(m<n+5):
 		for i in range(1, 50, 1):
 			em[i] = contentTree.xpath('//*[@id="ranklist"]/tbody/tr[' + str(i) + ']/td[1]/text()')
 			em1[i] = contentTree.xpath('//*[@id="ranklist"]/tbody/tr[' + str(i) + ']/td[2]/a/text()')
-			print em[i][0]+' '+em1[i][0]+'you are not here'
+			print em[i][0]+' '+em1[i][0]+"   It's not you"
 			if(em1[i][0]==q):
 				em2[i] = contentTree.xpath('//*[@id="ranklist"]/tbody/tr[' + str(i) + ']/td[4]/text()')
 				em3[i] = contentTree.xpath('//*[@id="ranklist"]/tbody/tr[' + str(i) + ']/td[5]/text()')
