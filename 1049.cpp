d5420b07bee76b72e2ab09d8bf71595c91bc1037
ghostlo <710340998@qq.com>
2018-03-11 13:07:00 +0800

    Create 1049.cpp

---
+++ 1049.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+int main(){
+    int n;
+    scanf("%d",&n);
+    getchar();
+    int i;
+    double a;
+    double sum=0.0;
+    for(i=0;i<n;i++){
+        scanf("%lf",&a);
+        sum += a*(n-i)*(i+1);
+    }
+    printf("%.2f",sum);
+    return 0;
+}
